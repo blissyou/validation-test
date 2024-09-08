fe115c51de3f3b3471ac095ae247e3b6b31ab0d0
원형 <147372294+blissyou@users.noreply.github.com>
2024-09-08 20:31:18 +0900

    validation 구현

---
+++ src/main/java/org/example/validation/controller/UserApiController.java
@@ -0,0 +1,38 @@
+package org.example.validation.controller;
+
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.example.validation.model.Api;
+import org.example.validation.model.UserRegisterRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/user")
+public class UserApiController {
+    @PostMapping("")
+    public Api<UserRegisterRequest> register(
+            @Valid
+            @RequestBody
+            Api<UserRegisterRequest> userRegisterRequest
+    ){
+        log.info("init : {}",userRegisterRequest);
+        var body = userRegisterRequest.getData();
+        var response = Api.<UserRegisterRequest>builder()
+                .resultCode(String.valueOf(HttpStatus.OK.value()))
+                .resultMessage(HttpStatus.OK.getReasonPhrase())
+                .data(body)
+                .build();
+        return response;
+    }
+}
+
